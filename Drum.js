3c299105c1789381a1be574bfcf6f30896a48cc1
Adrian Danlos <adrianf1team@gmail.com>
2019-02-25 19:55:00 +0100

    update

--- Drum.js
+++ Drum.js
@@ -1,93 +1,77 @@
-//Instead of targeting the html we can target directly from JS like this:
-/*document.getElementById("mybutton").onclick = function(event) { ...
-}*/
-//No hacer todo variables globales.
+//HTML elements
 var keyEl = document.getElementsByClassName("drumKey");
 var pEl = document.getElementsByTagName("p");
 var buttonEl = document.getElementById("power");
 var volumeEl = document.getElementById("volume");
 var sliderEl = document.getElementById("myRange");
-//BACKGROUND COLOR CHANGE ONCLICK
-for (i = 0; i < keyEl.length; i++) {
-    keyEl[i].addEventListener("click", colorChange(i));
-}
-//I don't understand why we are returning a value and why we dont need to return it if we don't pass an argument
-function colorChange(i) {
-    return function() {
-        swapColors(i);
-    }
-}
-//BACKGROUND COLOR CHANGE + SOUND ONKEYDOWN
-window.addEventListener("keydown", checkKeyPressed);
-//Intentar crear un objeto en lugar de 8 variables
-var audio = new Audio("Sounds/adrian1.mp3");
-var audio1 = new Audio("Sounds/casti1.mp3");
-var audio2 = new Audio("Sounds/jorge1.mp3");
-var audio3 = new Audio("Sounds/joel1.mp3");
-var audio4 = new Audio("Sounds/adrian2.mp3");
-var audio5 = new Audio("Sounds/casti2.mp3");
-var audio6 = new Audio("Sounds/jorge2.mp3");
-var audio7 = new Audio("Sounds/joel2.mp3");
+//PLAY SOUND + BACKGROUND COLOR CHANGE ON KEY EVENT
+var audio = new Audio("Sounds/do1.wav");
+var audio1 = new Audio("Sounds/re.wav");
+var audio2 = new Audio("Sounds/mi.wav");
+var audio3 = new Audio("Sounds/fa.wav");
+var audio4 = new Audio("Sounds/sol.wav");
+var audio5 = new Audio("Sounds/la.wav");
+var audio6 = new Audio("Sounds/si.mp3");
+var audio7 = new Audio("Sounds/do2.wav");
+var down = window.addEventListener("keydown", checkKeyPressed);
+var up = window.addEventListener("keyup", checkKeyPressed)
 
 function checkKeyPressed(e) {
-    if (buttonEl.textContent == "ON") {
+    if (buttonEl.textContent.trim() == "ON") {
         switch (e.keyCode) {
             case 65:
-                playSound();
-                swapColors(0);
+                keyActions(e, audio, 0);
                 break;
             case 83:
-                playSound();
-                swapColors(1);
+                keyActions(e, audio1, 1);
                 break;
             case 68:
-                playSound();
-                swapColors(2);
+                keyActions(e, audio2, 2);
                 break;
             case 70:
-                playSound();
-                swapColors(3);
+                keyActions(e, audio3, 3);
                 break;
             case 71:
-                playSound();
-                swapColors(4);
+                keyActions(e, audio4, 4);
                 break;
             case 72:
-                playSound();
-                swapColors(5);
+                keyActions(e, audio5, 5);
                 break;
             case 74:
-                playSound();
-                swapColors(6);
+                keyActions(e, audio6, 6);
                 break;
             case 75:
-                playSound();
-                swapColors(7);
+                keyActions(e, audio7, 7);
                 break;
         }
     }
 }
 
-function playSound(){
-    audio.volume = sliderEl.value * 0.01;
-    audio.play();
-    audio.currentTime = 0;
-}
-
-function swapColors(i) {
-    if (buttonEl.textContent.trim() == "ON") {
-        if (keyEl[i].style.backgroundColor == "rgba(0, 103, 73, 0.4)") {
-            keyEl[i].style.backgroundColor = "rgba(10, 03, 73, 0.4)";
-        } else {
-            keyEl[i].style.backgroundColor = "rgba(0, 103, 73, 0.4)";
-        }
+function keyActions(e, audio, i) {
+    if (e.type === "keydown") {
+        playSound(audio);
+        swapToBlue(i);
+    } else if (e.type === "keyup") {
+        swapToGreen(i);
     }
 }
-//Button POWER
-//event listeners
+
+function playSound(audioNumber) {
+    audioNumber.volume = sliderEl.value * 0.01;
+    audioNumber.play();
+    audioNumber.currentTime = 0;
+}
+
+function swapToBlue(i) {
+    keyEl[i].style.backgroundColor = "rgba(10, 03, 73, 0.4)";
+}
+
+function swapToGreen(i) {
+    keyEl[i].style.backgroundColor = "rgba(0, 103, 73, 0.4)";
+}
+//BUTTON POWER BUTTON POWER BUTTON POWER BUTTON POWER BUTTON POWER BUTTON POWER BUTTON POWER 
 buttonEl.addEventListener("click", turnOn);
 window.addEventListener("keydown", checkEnterKey);
-//Storing values
 var greenButton = function() {
     buttonEl.style.backgroundColor = "rgba(0, 103, 73, 0.4)";
     buttonEl.style.color = "white";
@@ -103,7 +87,7 @@ function turnOn(e) {
     if (buttonEl.textContent.trim() == "OFF") {
         greenButton();
         for (i = 0; i < keyEl.length; i++) {
-            keyEl[i].style.backgroundColor = "rgba(0, 103, 73, 0.4)";
+            swapToGreen(i);
             pEl[i].style.color = "white";
         }
     } else {
@@ -120,15 +104,21 @@ function checkEnterKey(e) {
         turnOn();
     }
 }
-//VOLUME SLIDER
+//VOLUME SLIDER VOLUME SLIDER VOLUME SLIDER VOLUME SLIDER VOLUME SLIDER VOLUME SLIDER 
 sliderEl.addEventListener("input", volumeUpdate);
 
 function volumeUpdate() {
     var val = sliderEl.value;
     volumeEl.innerHTML = "VOLUME " + val + "%";
 }
-/*sliderEl.addEventListener("mouseover", showVolume);
+//VISIBILITY HIDDEN
+sliderEl.addEventListener("mousedown", showVolume);
+sliderEl.addEventListener("mouseup", hideVolume);
 
-function volumeUpdate() {
+function showVolume() {
     volumeEl.style.visibility = 'visible';
-}*/
+}
+
+function hideVolume() {
+    volumeEl.style.visibility = 'hidden';
+}
