1bf440ac1ebb207403fbd122ad42573201220217
AdrianDanlos <danlosadrian@gmail.com>
2019-12-20 20:07:24 +0100

    new account

--- funciones.js
+++ funciones.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    let api = 'https://cors-anywhere.herokuapp.com/https://api.mozambiquehe.re/bridge?platform=PC&player=N3Essential,N3EssentialSmurf,EssentialReborn,spacexfanboy,asiatristanvigo&auth=';
+    let api = 'https://cors-anywhere.herokuapp.com/https://api.mozambiquehe.re/bridge?platform=PC&player=N3Essential,N3EssentialSmurf,EssentialReborn,spacexfanboy,asiatristanvigo,thechinesesoul&auth=';
     let key = 'LPuQwxrvLY7hspWf1eST';
 
     //First call
