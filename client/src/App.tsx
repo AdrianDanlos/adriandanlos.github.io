20f2b2403843de68b9efb43c1644c9a63c229f30
AdrianDanlos <adrianf1team@gmail.com>
2022-10-16 04:11:09 +0700

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data));
   }, []);
 
   console.log(data);
--- server/index.js
+++ server/index.js
@@ -8,17 +8,22 @@ const request = require("request");
 
 app.get("/api", (req, res) => {
   //   res.json({ message: "Hello from server!" });
+  let data = "empty";
   request(
-    "https://coinlib.io/api/v1/global?key=XXX&pref=EUR",
+    "https://coinlib.io/api/v1/coinlist?key=2d5f375478a9fe88&pref=USD&page=1&order=rank_asc",
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
+        console.log("success");
         console.log(body);
+        return;
       }
+      console.log("error");
       console.log(error);
       console.log(response.statusCode);
       console.log(response.body);
     }
   );
+  res.json(data);
 });
 
 app.listen(PORT, () => {
